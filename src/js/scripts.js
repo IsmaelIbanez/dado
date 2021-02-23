c1ef755dddfdae9df62e4fbf8bf1d3b727ab2e7f
Ismael Ibanez <ismaelibanezdev@gmail.com>
2021-02-23 20:30:37 +0000

    cositis done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,62 +1,71 @@
 let dinero = parseInt(document.querySelector('.dinero').textContent);
 let anterior = 1;
+let apuesta = parseInt(document.querySelector('.apuesta').value);
+let historial = [];
+
 document.querySelector("button").addEventListener("click", tirar);
 
 
 function tirar() {
-    document.querySelector('.mensaje').innerHTML = " ";
-    if (dinero > 0) {
 
-        let apuesta = parseInt(document.querySelector('.apuesta').value);
+    document.querySelector('.mensaje').textContent = " ";
 
-        if (dinero >= apuesta) {
-
-            let ran = Math.ceil(Math.random() * 6);
-            document.querySelector('.dinero').innerHTML = dinero;
-
-            if (ran <= anterior) {
-                dinero -= apuesta;
-                document.querySelector('.dinero').innerHTML = dinero;
-            } else {
-                dinero += apuesta
-                document.querySelector('.dinero').innerHTML = dinero;
-            }
-
-            anterior = ran;
-
-            switch (ran) {
-                case 1:
-                    document.querySelector('.dado').src = 'img/dice1.gif';
-                    break;
-
-                case 2:
-                    document.querySelector('.dado').src = 'img/dice2.gif';
-                    break;
-
-                case 3:
-                    document.querySelector('.dado').src = 'img/dice3.gif';
-                    break;
-
-                case 4:
-                    document.querySelector('.dado').src = 'img/dice4.gif';
-                    break;
-
-                case 5:
-                    document.querySelector('.dado').src = 'img/dice5.gif';
-                    break;
-
-                case 6:
-                    document.querySelector('.dado').src = 'img/dice6.gif';
-                    break;
-            }
-        } else {
-            document.querySelector('.dinero').innerHTML = dinero + " No tienes tanto dinero";
+    if (dinero > 0 && dinero >= apuesta && typeof(apuesta) == true ) {
+        console.log("suuu");
+        seJuega();       
+    } 
+    else {
+        if (dinero < apuesta){
+            document.querySelector('.dinero').textContent = dinero + " No tienes tanto dinero";
+        }else{
+            document.querySelector('.dado').src = 'img/dice1.gif';
+            document.querySelector('.mensaje').innerHTML = "<br>¡Se te acabó el dinero! Vuelve a intentarlo";
+            dinero = 10;
+            document.querySelector('.dinero').textContent = dinero;
         }
+    }
+}
 
+function seJuega() { 
+    console.log("suuuka");
+    let random = Math.ceil(Math.random() * 6);
+    historial.push(random);
+    document.querySelector('.historial').textContent = historial;
+    document.querySelector('.dinero').textContent = dinero;
+
+    if (random <= anterior) {
+        dinero -= apuesta;
+        document.querySelector('.dinero').textContent = dinero;
     } else {
-        document.querySelector('.dado').src = 'img/dice1.gif';
-        document.querySelector('.mensaje').innerHTML = "<br>¡Se te acabó el dinero! Vuelve a intentarlo";
-        dinero = 10;
-        document.querySelector('.dinero').innerHTML = dinero;
+        dinero += apuesta
+        document.querySelector('.dinero').textContent = dinero;
+    }
+
+    anterior = random;
+
+    switch (random) {
+        case 1:
+            document.querySelector('.dado').src = 'img/dice1.gif';
+            break;
+
+        case 2:
+            document.querySelector('.dado').src = 'img/dice2.gif';
+            break;
+
+        case 3:
+            document.querySelector('.dado').src = 'img/dice3.gif';
+            break;
+
+        case 4:
+            document.querySelector('.dado').src = 'img/dice4.gif';
+            break;
+
+        case 5:
+            document.querySelector('.dado').src = 'img/dice5.gif';
+            break;
+
+        case 6:
+            document.querySelector('.dado').src = 'img/dice6.gif';
+            break;
     }
 }
